66c7f292a10965021aa41c6ae96bdf977895a97f
muruganantham <muruganantham2399@gmail.com>
2023-12-03 18:15:10 +0530

    initial-push

---
+++ src/components/PharmacyCart.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../style/PharmacyCart.css";
+
+const PharmacyCart = () => {
+  return (
+    <div className="card-head">
+      <div className="card-style">
+        <div className="card-title">
+          <div>Online pharmacy</div>
+          <div>Mobile application</div>
+        </div>
+        <div className="sub-section">
+          <div>Client name : BlueKode solutions</div>
+          <div>Project number : 6</div>
+        </div>
+      </div>
+      <div className="bottom-content">Status : Completed</div>
+    </div>
+  );
+};
+
+export default PharmacyCart;
